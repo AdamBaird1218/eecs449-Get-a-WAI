cd8e36250a5b9ea5be3321f886da83cac1dd1f38
Adam Baird <bairda@umich.edu>
2023-02-26 16:04:35 -0500

    Frontend text box and conversation display working

--- frontend/get_a_wai_react/src/App.js
+++ frontend/get_a_wai_react/src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
-
+import "react-chat-elements/dist/main.css";
+import { MessageBox} from "react-chat-elements";
+import React, { useState } from 'react';
 function App() {
+  const [text_entry, setText_entry] = useState('')
+  const [message_list, setMessage_list] = useState(["temp", "2_entries"])
+
+  const handleSubmit = () => {
+    setText_entry('');
+    setMessage_list(prev_list => [...prev_list, text_entry])};
+
   return (
+    <>
     <div className="App">
-      <h1>Hello World!</h1>
+      <MessageBox
+      position={"left"}
+      type={"text"}
+      title={"Message Box Title"}
+      text="Welcome to Get-A-WAI! What do you like to do?"
+      />
     </div>
+    <div>
+        {message_list.map(text_list_item =>{
+          return (<MessageBox
+            position={"right"}
+            type={"text"}
+            title={"Me"}
+            text={text_list_item}
+          />)
+        })}
+    </div>
+        
+
+    <div>
+          <input type="text" name="user_question" value={text_entry} onChange={e => setText_entry(e.target.value)} />
+          <button onClick={handleSubmit} > Submit ME!</button>
+       
+    </div>
+  </>
   );
 }
 
