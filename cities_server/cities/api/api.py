cdc337d2fd7210df5846306df3d91f1ce743a7d0
Adam Baird <bairda@umich.edu>
2023-03-12 23:10:09 -0400

    started work on cities filter

--- cities_server/cities/api/api.py
+++ cities_server/cities/api/api.py
@@ -1,5 +1,6 @@
 import flask
 import cities
+import spacy
 
 
 @cities.app.route('/api/', methods=['GET'])
@@ -11,8 +12,9 @@ def get_services():
     }
     return flask.jsonify(**context)
 
-def getCities(activity1,activity2,activity3):
-    connection = cities.model.getDb()
+def getCities(activity_list):
+    connection = cities.model.get_db()
+    act1, act2, act3 = filter_activities(activity_list)
     curr = connection.execute("SELECT C.city_name, A.activity_name "
    "FROM Cities C "
    "INNER JOIN City_Activities CA "
@@ -21,7 +23,29 @@ def getCities(activity1,activity2,activity3):
    "ON CA.activity_id = A.activity_id "
    "WHERE A.activity_name = ? OR A.activity_name = ? OR A.activity_name = ?"
    "LIMIT 5"
-   ,(activity1,activity2,activity3)
+   ,(act1,act2,act3)
     )
     cities = curr.fetchall()
     return cities
+
+def filter_activities(con, act_list):
+    nlp = spacy.load('en_core_web_md')
+    activites_list = get_all_activites(con)
+    filtered_acts = []
+    for usr_act in act_list: 
+        for db_act in activites_list:
+            
+            pass
+        
+    
+    
+def get_all_activites(con):
+    curr = con.execute(
+        "SELECT DISTINCT A.activity_name "
+        "FROM Activities A"
+    )
+    return curr.fetchall()
+    
+    
+def convert(lst):
+    return str(lst).translate(None, '[],\'')
